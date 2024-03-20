a71b4f98a742401011cb2e2acc0b12398be5e2be
ThomasvHoutum <thomasvanhoutum@hotmail.com>
2024-03-20 19:55:09 +0100

    space

--- Domain/Models/LoginDetail.cs
+++ Domain/Models/LoginDetail.cs
@@ -10,7 +10,7 @@ namespace Domain.Models
         
         public string Email { get; private set; }
         
-        public string Password { get; private set;}
+        public string Password { get; private set; }
         
         public List<SecurityQuestion> SecurityQuestions { get; private set; }
 
