de6cb93abe5cb4e9e6ca79089303caecb0cb24bd
ThomasvHoutum <thomasvanhoutum@hotmail.com>
2024-05-27 19:05:03 +0200

    Comments

--- Domain/Managers/SearchManager.cs
+++ Domain/Managers/SearchManager.cs
@@ -21,10 +21,13 @@ public class SearchManager
             var properties = entry.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
             foreach (var property in properties)
             {
+                // Check if the property is a string or a value type
                 if (property.PropertyType != typeof(string) && !property.PropertyType.IsValueType) 
                     continue;
                 
                 var value = property.GetValue(entry);
+                
+                // Check if the value contains the search query
                 if (value != null && value.ToString().ToLower().Contains(searchQuery.ToLower()))
                     return true;
             }
