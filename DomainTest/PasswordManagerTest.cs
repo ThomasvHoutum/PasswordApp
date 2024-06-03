a2c6c16c4f6f9f62c7420e11729f9d94bafed56d
ThomasvHoutum <thomasvanhoutum@hotmail.com>
2024-06-03 13:53:46 +0200

    test for adding with exception

--- DomainTest/PasswordManagerTest.cs
+++ DomainTest/PasswordManagerTest.cs
@@ -23,5 +23,18 @@ namespace Domain.Test
             var entries = passwordManager.LoginDetailRepository.Entries;
             Assert.Contains(entries, entry => entry.Username == "John Doe" && entry.Password == "password123");
         }
+        
+        [Fact]
+        public void AddLoginDetail_ShouldNotAddEntryWithInvalidUsername()
+        {
+            // Arrange
+            var passwordManager = new PasswordManager(new FileHelper(), new EncryptionHelper());
+            
+            // Login detail with invalid username
+            var loginDetail = new LoginDetail("Google", "", "john@example.com", "password123", new List<SecurityQuestion>());
+
+            // Assert
+            Assert.Throws<ArgumentException>(() => passwordManager.LoginDetailRepository.Add(loginDetail));
+        }
     }
 }
