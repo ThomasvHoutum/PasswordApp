c3b14140ef9c4f7ab89f06b07a1c2a18a62f700e
ThomasvHoutum <thomasvanhoutum@hotmail.com>
2024-06-04 19:52:30 +0200

    Testing all account entries on adding to repository

--- DomainTest/PasswordManagerTest.cs
+++ DomainTest/PasswordManagerTest.cs
@@ -9,6 +9,8 @@ namespace DomainTest
 {
     public class PasswordManagerTest
     {
+        #region LoginDetail
+        
         [Fact]
         public void AddLoginDetail_ShouldAddEntry()
         {
@@ -36,5 +38,71 @@ namespace DomainTest
             // Assert
             Assert.Throws<ArgumentException>(() => passwordManager.LoginDetailRepository.Add(loginDetail));
         }
+        
+        #endregion
+        
+        #region BankDetail
+        
+        [Fact]
+        public void AddBankDetail_ShouldAddEntry()
+        {
+            // Arrange
+            var passwordManager = new PasswordManager(new FileHelper(), new EncryptionHelper());
+            var bankDetail = new BankDetail("ING", 1111111111, "John Doe", "password123");
+
+            // Act
+            passwordManager.BankDetailRepository.Add(bankDetail);
+
+            // Assert
+            var entries = passwordManager.BankDetailRepository.Entries;
+            Assert.Contains(entries, entry => entry.Username == "John Doe" && entry.Password == "password123");
+        }
+        
+        [Fact]
+        public void AddBankDetail_ShouldNotAddEntryWithInvalidUsername()
+        {
+            // Arrange
+            var passwordManager = new PasswordManager(new FileHelper(), new EncryptionHelper());
+            
+            // Login detail with invalid username
+            var bankDetail = new BankDetail("", 1111111111, "John Doe", "password123");
+
+            // Assert
+            Assert.Throws<ArgumentException>(() => passwordManager.BankDetailRepository.Add(bankDetail));
+        }
+        
+        #endregion
+        
+        #region CreditCard
+        
+        [Fact]
+        public void AddCreditCard_ShouldAddEntry()
+        {
+            // Arrange
+            var passwordManager = new PasswordManager(new FileHelper(), new EncryptionHelper());
+            var creditCard = new CreditCard("John Doe", 1111111111, DateTime.Now, 1, "address123");
+
+            // Act
+            passwordManager.CreditCardRepository.Add(creditCard);
+
+            // Assert
+            var entries = passwordManager.CreditCardRepository.Entries;
+            Assert.Contains(entries, entry => entry.Issuer == "John Doe" && entry.BillingAddress == "address123");
+        }
+        
+        [Fact]
+        public void AddCreditCard_ShouldNotAddEntryWithInvalidIssuer()
+        {
+            // Arrange
+            var passwordManager = new PasswordManager(new FileHelper(), new EncryptionHelper());
+            
+            // Login detail with invalid username
+            var creditCard = new CreditCard("", 1111111111, DateTime.Now, 1, "address123");
+
+            // Assert
+            Assert.Throws<ArgumentException>(() => passwordManager.CreditCardRepository.Add(creditCard));
+        }
+        
+        #endregion
     }
 }
